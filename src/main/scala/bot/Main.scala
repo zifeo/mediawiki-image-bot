7d46882e1755e8830de97121cc8ebc587dd89f0e
Teo Stocco <teo.stocco@epfl.ch>
2016-04-12 15:37:43 +0200

    add conf files

--- src/main/scala/bot/Main.scala
+++ src/main/scala/bot/Main.scala
@@ -5,6 +5,13 @@ import net.sourceforge.jwbf.mediawiki.bots.MediaWikiBot
 object Main extends App {
 
   println("hello")
+  val bot = new MediaWikiBot(config.getString("mediawiki"))
 
+  println(bot.getSiteinfo)
+
+  val article = bot.getArticle("Bots")
+  println(article.getText)
+  //bot.login(config.getString("login"), config.getString("password"))
+  //article.save()
 
 }
---
+++ src/main/scala/bot/package.scala
@@ -0,0 +1,12 @@
+import java.util.logging.LogManager
+
+import com.typesafe.config.ConfigFactory
+import org.slf4j.LoggerFactory
+
+package object bot {
+
+  LogManager.getLogManager.readConfiguration()
+  val config = ConfigFactory.load().getConfig("bot")
+  val log = LoggerFactory.getLogger("bot")
+
+}
