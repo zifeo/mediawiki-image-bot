a19f01fe2b2e45d99b7512d01bc538107c0179fc
Teo Stocco <teo.stocco@epfl.ch>
2016-05-03 13:26:52 +0200

    reformat

--- src/main/scala/bot/WikiImage.scala
+++ src/main/scala/bot/WikiImage.scala
@@ -3,20 +3,20 @@ package bot
 import bot.IOUtils._
 
 class WikiImage(
-                 val snippet: String,
-                 val url: String,
-                 val thumbnail: String,
-                 var filename: String
+                 snippet: String,
+                 url: String,
+                 thumbnail: String,
+                 filename: String
                ) {
 
   override def toString: String = {
     s"""
-       |        {
-       |          "url": ${safeString(url)},
-       |          "snippet": ${safeString(snippet)},
-       |          "thumbnail": ${safeString(thumbnail)},
-       |          "filename": ${safeString(filename)}
-       |        }
+       |{
+       |  "url": ${safeString(url)},
+       |  "snippet": ${safeString(snippet)},
+       |  "thumbnail": ${safeString(thumbnail)},
+       |  "filename": ${safeString(filename)}
+       |}
     """.stripMargin
   }
 
