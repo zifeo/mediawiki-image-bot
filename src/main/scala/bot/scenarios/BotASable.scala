cad0ab3fc8882542c32e6823af3daf3d34477a8f
JBouron <justinien.bouron@epfl.ch>
2016-05-09 17:23:54 +0200

    Implement a first version of mixing results from Google and Flickr according to the average date of the page.

--- src/main/scala/bot/scenarios/BotASable.scala
+++ src/main/scala/bot/scenarios/BotASable.scala
@@ -1,19 +1,77 @@
 package bot.scenarios
 
 import bot._
-import bot.providers.FlickrSearch
+import bot.providers.{GoogleSearch, FlickrSearch}
+import bot.utils.Tokenizer
+import bot.wiki.PageType
 
 object BotASable extends BotApp {
 
   bot.signIn()
-  val p = bot.load("BotASable")
 
-  val res = FlickrSearch("Matrix")
+  val allowedPageTypes = List(PageType.UNCLASSIFIED, PageType.LITERAL)
+  val images = bot.allWikiPages.filter(p => allowedPageTypes.contains(p.pageType)).map(page => {
+    val pageArticle = bot.getArticle(page.title)
+    print("Page " + page.title)
+    val yearsInPage = Tokenizer.year(pageArticle)
+    val pageImage = if (yearsInPage.nonEmpty) {
+      val average = yearsInPage.sum / yearsInPage.size
+      if (average < config.getInt("yearThreshold")) {
+        print(" search on Google : ")
+        val res = GoogleSearch(page.title)
+        if (res.isEmpty) {
+          println("no results")
+          FlickrSearch(page.title)
+        }
+        else {
+          println("ok")
+          res
+        }
+      }
+      else {
+        print(" search Flickr : ")
+        val res = FlickrSearch(page.title)
+        if (res.isEmpty){
+          println("no results")
+          GoogleSearch(page.title)
+        }
+        else {
+          println("ok")
+          res
+        }
+      }
+    }
+    else {
+        print(" search on Default : ")
+        val res = GoogleSearch(page.title)
+        if (res.isEmpty) {
+          println("no results")
+          FlickrSearch(page.title)
+        }
+        else {
+          println("ok")
+          res
+        }
+    }
+    (page.title, pageImage)
+  })
 
-  bot.add(p.get, res.head._1, res.head._2)
+  val results = images.take(20).toList
+  results.foreach(res => {
+    print(res._1 + " : ")
+    res._2.foreach(im => print(im._1.url + "  ,  "))
+    println("\n\n")
+  })
 
-  println(res)
 
+  /*
 
+  println("Flickr result : ")
+  resFlickr.foreach(_._1.print())
+  println("Google result : ")
+  resGoogle.foreach(_._1.print())
 
+  println("\n\nTotal resutlts :")
+  println("\tFilckr : " + resFlickr.size)
+  println("\tGoogle : " + resGoogle.size)*/
 }
