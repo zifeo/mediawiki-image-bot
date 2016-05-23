7c863ef497faf0acfae522aded6a3996bf1cb344
JBouron <justinien.bouron@epfl.ch>
2016-05-23 18:22:28 +0200

    Break master !

--- src/main/scala/bot/scenarios/Evaluation.scala
+++ src/main/scala/bot/scenarios/Evaluation.scala
@@ -18,20 +18,46 @@ object Evaluation extends BotApp {
     .allWikiPages
     .filter(p => allowedPageTypes.contains(p.pageType))
     .map { page =>
+      try {
+        val pageArticle = bot.getArticle(page.title)
+        print("Page " + page.title)
 
-      val pageArticle = bot.getArticle(page.title)
-      print("Page " + page.title)
+        val yearsInPage = Tokenizer.year(pageArticle)
 
-      val yearsInPage = Tokenizer.year(pageArticle)
+        val pageImage =
+          if (yearsInPage.nonEmpty) {
+            val average = yearsInPage.sum / yearsInPage.size
 
-      val pageImage =
-        if (yearsInPage.nonEmpty) {
-          val average = yearsInPage.sum / yearsInPage.size
+            if (average < config.getInt("yearThreshold")) {
+              print(" search on Google : ")
 
-          if (average < config.getInt("yearThreshold")) {
-            print(" search on Google : ")
+              val res = GoogleSearch(page.title)
+              if (res.isEmpty) {
+                println("no results")
+                FlickrSearch(page.title)
+              } else {
+                println("ok")
+                res
+              }
 
+            } else {
+              print(" search Flickr : ")
+              val res = FlickrSearch(page.title)
+
+              if (res.isEmpty) {
+                println("no results")
+                GoogleSearch(page.title)
+              }
+              else {
+                println("ok")
+                res
+              }
+            }
+
+          } else {
+            print(" search on Default : ")
             val res = GoogleSearch(page.title)
+
             if (res.isEmpty) {
               println("no results")
               FlickrSearch(page.title)
@@ -39,44 +65,31 @@ object Evaluation extends BotApp {
               println("ok")
               res
             }
-
-          } else {
-            print(" search Flickr : ")
-            val res = FlickrSearch(page.title)
-
-            if (res.isEmpty){
-              println("no results")
-              GoogleSearch(page.title)
-            }
-            else {
-              println("ok")
-              res
-            }
           }
-
-        } else {
-          print(" search on Default : ")
-          val res = GoogleSearch(page.title)
-
-          if (res.isEmpty) {
-            println("no results")
-            FlickrSearch(page.title)
-          } else {
-            println("ok")
-            res
-          }
-        }
-      (page, pageImage)
+        (page, pageImage)
+      }
+      catch {
+        case _ : Exception => (None, None)
+      }
     }
-
-  images.take(30).foreach {
+  var img_count = 0
+  images.foreach {
     //case (_, Stream.empty) =>
+    case (None, None) =>
     case (page, im) =>
-      if (im.nonEmpty) {
-        val (_image, _file) = im.head
-        bot.add(page, _image, _file)
-        println("Add image for " + page.title + " with image " + _image.url)
+      try {
+        if (im.nonEmpty) {
+          val (_image, _file) = im.head
+          bot.add(page, _image, _file)
+          println("Add image for " + page.title + " with image " + _image.url)
+          img_count += 1
+          println(img_count + " Image added until now")
+        }
+      }
+      catch {
+        case _ : Exception => println("## ERROR ## page " + page.title)
       }
   }
-
+  println("\n\n################### END #################")
+  println(img_count + " Image added in total")
 }
